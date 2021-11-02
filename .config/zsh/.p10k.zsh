cc7626c333187da156ebb5a8b280d751b0b5c9ef
yeetologist <makanmakan9c@gmail.com>
2021-11-02 15:54:26 +0700

    remove battery indicator

--- .config/zsh/.p10k.zsh
+++ .config/zsh/.p10k.zsh
@@ -31,7 +31,8 @@
 
   # The list of segments shown on the left. Fill it with the most important segments.
   typeset -g POWERLEVEL9K_LEFT_PROMPT_ELEMENTS=(
-    battery               # internal battery
+    time                    # current time
+    # battery               # internal battery
     # os_icon                 # os identifier
     dir                     # current directory
     vcs                     # git status
@@ -95,7 +96,6 @@
     # todo                    # todo items (https://github.com/todotxt/todo.txt-cli)
     # timewarrior             # timewarrior tracking status (https://timewarrior.net/)
     # taskwarrior             # taskwarrior task count (https://taskwarrior.org/)
-    time                    # current time
     # ip                    # ip address and bandwidth usage for a specified network interface
     # public_ip             # public IP address
     # proxy                 # system-wide http/https/ftp proxy
